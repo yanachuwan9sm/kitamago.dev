3bb9e860c145423e2b906dd0ebaa9d23768299f0
yanachuwan9sm <gurem919@icloud.com>
2022-04-22 00:08:27 +0900

    setting: Google Analytics 用の型ファイルをインストール

---
+++ src/libs/gtag.ts
@@ -0,0 +1,24 @@
+export const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || "";
+
+// PVを測定
+export const pageview = (path: string) => {
+  if (GA_ID === undefined) {
+    return;
+  }
+  window.gtag("config", GA_ID, {
+    page_path: path,
+  });
+};
+
+// GAイベントを発火させる
+export const event = ({ action, category, label, value = "" }) => {
+  if (!existsGaId) {
+    return;
+  }
+
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: JSON.stringify(label),
+    value,
+  });
+};
