446584f235679331a7e5754b548efbd730a09395
yanachuwan9sm <gurem919@icloud.com>
2022-07-29 23:07:27 +0900

    - setting : ⚙ styled-componentsパッケージの導入 & config.js に記述追加

--- next.config.js
+++ next.config.js
@@ -1,23 +1,22 @@
 /** @type {import('next').NextConfig} */
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 });
 
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
   images: {
     // URLのホスト名を追加 (next/image)
-    domains: ["images.microcms-assets.io"],
+    domains: ['images.microcms-assets.io'],
   },
-
 };
 
-if(process.env.ANALYZE === "true"){
+if (process.env.ANALYZE === 'true') {
   module.exports = withBundleAnalyzer({ nextConfig });
-}
-else{
+} else {
   module.exports = nextConfig;
 }
-
-
